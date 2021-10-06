ecc2946e386f6003e834b34376eedaacf8c933cd
luiscarlos55 <a01254044@itesm.mx>
2021-10-06 17:20:15 +0000

    Tarea 6

--- assignments/09SerieFibonacciListas/src/exercise.py
+++ assignments/09SerieFibonacciListas/src/exercise.py
@@ -1,6 +1,20 @@
 def main():
-    
-    pass
+    lista=[]
+    numero=-1
+    while numero<0:
+        numero=int(input())
+    if numero==0:
+        print('[]')
+    elif numero>0:
+        lista=[0,1]
+        if numero>2:
+            for i in range(numero-2):
+                lista.append(lista[i]+lista[i+1])
+            print(lista)
+        elif numero==1:
+            print('[0]')
+        elif numero==2:
+            print(lista)
 
 if __name__=='__main__':
     main()
